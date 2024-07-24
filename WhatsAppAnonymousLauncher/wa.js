50cebbb2827f6f0a9c1ae13b071693c11c02eeec
theDontKnowGuy <40746333+theDontKnowGuy@users.noreply.github.com>
2024-07-24 11:04:20 +0300

    Add files via upload

--- WhatsAppAnonymousLauncher/wa.js
+++ WhatsAppAnonymousLauncher/wa.js
@@ -1,33 +1,25 @@
 function isValidNumber() {
-    let n = document.getElementById("waNumber").value;
-    n = n.replace(/[^a-zA-Z0-9() ]/g, '');
-    if (n.search("9720") == 0) n = n.replace("972", "");
-    if (n.search("0") == 0) n = n.replace("0", "972");
-    if (n.length>=12) {
-    var url = "whatsapp://send/?phone=" + n + '&text&app_absent=0';
-    var windowObjectReference;
-    windowObjectReference = window.open(url);
-    console.log(n);
-    return true;
-    }
-    else
-    {   
-      document.getElementById("em1").style.visibility='visible';
-      return false;
-    }
+  let n = document.getElementById("waNumber").value;
+  n = n.replace(/[^a-zA-Z0-9]/g, '');
+  if (n.search("0") == 0) n = n.replace("0", "972");
+  var url = "https://wa.me/" + n;
+
+  var windowObjectReference;
+  windowObjectReference = window.open(url);
+  console.log(n);
+  return true;
 }
 
-function clearErrorMessage(){
-    document.getElementById("em1").style.visibility='hidden';
+function clearErrorMessage() {
+  document.getElementById("em1").style.visibility = 'hidden';
 }
 
-document.getElementById('f1').onsubmit = function() {
+document.getElementById('f1').onsubmit = function () {
   return isValidNumber();
 };
 
 const im = document.getElementById("i1");
-im.addEventListener("click", isValidNumber,true);
+im.addEventListener("click", isValidNumber, true);
 
 const ip = document.getElementById("waNumber");
-ip.addEventListener("input", clearErrorMessage,false);
-
+ip.addEventListener("input", clearErrorMessage, false);
